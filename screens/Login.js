788fb9707e76f49abff3b935bc36468e439b3f52
Felipe Martins <felipe@Felipes-MacBook-Air.local>
2022-04-07 00:03:25 -0300

    adicionada imagem no fundo, print1.png para referencia

--- screens/Login.js
+++ screens/Login.js
@@ -2,24 +2,50 @@ import React from 'react';
 import {
     View,
     Text,
-    TouchableOpacity
+    TouchableOpacity,
+    ImageBackground,
+     StatusBar
 } from 'react-native';
 
+import LinearGradient   from 'react-native-linear-gradient'
+
+import { images, COLORS, SIZES, FONTS } from '../constants'
+
 const Login = ({ navigation }) => {
+
+    function renderHeader() {
+        return(
+            <View
+                style={{
+                    height: SIZES.height > 700 ? "65%" : "60%"
+                }}
+            >
+                <ImageBackground
+                    source={images.loginBackground}
+                    style={{
+                        flex: 1,
+                        justifyContent: "flex-end" 
+                    }}
+                >
+
+                </ImageBackground>
+            </View>
+        )
+    }
+
     return (
         <View
-            style={{
-                flex: 1,
-                alignItems: 'center',
-                justifyContent: 'center'
-            }}
+        style={{
+             flex: 1,
+             backgroundColor: COLORS.black
+        }}
         >
-            <Text>Login</Text>
-            <TouchableOpacity
-                onPress={() => navigation.replace("Home")}
-            >
-                <Text>Navigate to Home</Text>
-            </TouchableOpacity>
+
+        <StatusBar barStyle='light-content' />
+            {/* Header */}
+            {renderHeader()}
+            {/* Details */}
+
         </View>
     )
 }
