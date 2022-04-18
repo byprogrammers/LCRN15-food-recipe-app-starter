60cf5c74a4b8f2a491a2c17dc8ebc485ad4aabd4
Felipe Martins <felipemartins210@gmail.com>
2022-04-17 21:43:45 -0300

    styled header image on recipe section

--- screens/Recipe.js
+++ screens/Recipe.js
@@ -26,6 +26,45 @@ const Recipe = ({navigation, route}) => {
         setSelectedRecipe(recipe)
     }, [])
 
+    function renderRecipeCardHeader() {
+        return(
+            <View
+                style={{
+                    alignItems: 'center',
+                    overflow: 'hidden',
+                    marginTop: -1000,
+                    paddingTop: 1000
+                }}
+            >
+                {/* Background Image */}
+                <Animated.Image 
+                    source={selectedRecipe?.image}
+                    resizeMode='contain'
+                    style={{
+                        height: HEADER_HEIGHT,
+                        width: '200%',
+                        transform: [
+                            {
+                                translateY: scrollY.interpolate({
+                                    inputRange: [-HEADER_HEIGHT, 0, HEADER_HEIGHT],
+                                    outputRange: [-HEADER_HEIGHT / 2, 0, HEADER_HEIGHT * 0.75]
+                                })
+                            },
+                            {
+                                scale: scrollY.interpolate({
+                                    inputRange: [-HEADER_HEIGHT, 0, HEADER_HEIGHT],
+                                    outputRange: [2, 1, 0.75]
+                                })
+                            }
+                        ]
+                    }}
+                />
+                {/* Recipe Creator Card */}
+
+            </View>
+        )
+    }
+
     return (
         <View
             style={{
@@ -38,7 +77,15 @@ const Recipe = ({navigation, route}) => {
                 keyExtractor={item => `${item.id}`}
                 showsVerticalScrollIndicator={false}
                 ListHeaderComponent={
-                    <View></View>
+                    <View>
+                        {/* Header */}
+                        {renderRecipeCardHeader()}
+                        
+                        {/* Info */}
+
+                        {/* Ingredient Title */}
+
+                    </View>
                 }
                 scrollEventThrottle={16}
                 onScroll={Animated.event([
